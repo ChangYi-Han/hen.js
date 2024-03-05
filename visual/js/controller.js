86e822d4ea8d32c0853c08995e9b591b8bfc739f
Yi-Hen <88665055+ChangYi-Han@users.noreply.github.com>
2024-03-05 20:39:57 +0800

    Update controller.js

--- visual/js/controller.js
+++ visual/js/controller.js
@@ -478,11 +478,9 @@ $.extend(Controller, {
 
         centerX = Math.ceil(availWidth / 2 / nodeSize);
         centerY = Math.floor(height / 2 / nodeSize);
-        if (this.startNode) {
-        this.setStartPos(3, 4);
-        }
 
-        //this.setStartPos(3,4);//(centerX - 5, centerY);
+
+        this.setStartPos(3,4);//(centerX - 5, centerY);
         
         this.setWalkableAt(3, 1, false)
         this.setNikePos(4, 1)
